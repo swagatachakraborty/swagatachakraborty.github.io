4044a383f10452fb407181c41a7eed4945cea2bb
Swagata Chakraborty <44020073+swagatachakraborty@users.noreply.github.com>
2019-01-07 20:29:18 +0530

    Added a fill region tool to fill a region with color.
    the fill region section is still not working for large section. It is
    then exiting the maximum stack size issue.

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
 let random_color_clicked = true;
 let color;
 let drawing = false;
-let brush = false;
-let pencil = true;
+let pencil, brush;
 
 const random = (range) => Math.floor(Math.random() * range);
 
 const randomColor = () => 'rgb(' + random(255) + ',' + random(255) + ',' + random(255) + ')';
+
+const getElement = (id) => document.getElementById(id);
 
 const updateCurrentColor = selectedColor =>{
 	color = selectedColor;
@@ -14,7 +15,7 @@ const updateCurrentColor = selectedColor =>{
 }
 
 const changeBackgroundColor = function (id, color) {
-	let element = document.getElementById(id);
+	let element = getElement(id);
 	element.setAttribute('style', 'background-color: ' + color);
 };
 
@@ -30,10 +31,10 @@ const chooseRandomColor = function () {
 };
 
 const createBoard = function () {
-	let max = 142;
+	let max = 140;
 	let table = `<table
 	class=canvas
-	onclick='sratrOrStopDrawing()'
+	onclick='drawOrFill(event.target)'
 	onmousemove='draw(event.target)'>
 	<tbody>`;
 	for(let x = 0; x < max; x++){
@@ -50,19 +51,53 @@ const createBoard = function () {
 
 window.onload = function () {
 	document.getElementById('board_container').innerHTML += createBoard();
+	selectTool('pencil');
+	updateCurrentColor('black');
 };
 
-const sratrOrStopDrawing = function () {
+const updateColorByValue = function (id) {
+	let selectedColor = getElement(id).value;
+	updateCurrentColor(selectedColor);
+};
+
+const drawOrFill = function (pixel) {
+	startOrStopDrawing();
+	if(brush)	fill(pixel);
+};
+
+const fill = function (pixel) {
+	let oldColor = getElement(pixel.id).style.backgroundColor;	
+	fillRegion(pixel, oldColor);
+	drawing = false;
+};
+
+const startOrStopDrawing = function (pixel) {
 	drawing = !drawing;
 };
 
 const draw = function (pixel) {
-	if(pixel.id != "" && drawing){
-		changeBackgroundColor(pixel.id, color);
-	}
+	if(pixel.id == "" || !drawing)	return ;
+	changeBackgroundColor(pixel.id, color);
 };
 
-const updateColorByValue = function (id) {
-	let selectedColor = document.getElementById(id).value;
-	updateCurrentColor(selectedColor);
+const fillRegion = function (pixel, oldColor) {
+	let pixelColor = getElement(pixel.id).style.backgroundColor;
+	if(pixelColor != oldColor) return ;
+	draw(pixel);
+	return getAdjuscentPixels(pixel.id).forEach(point => fillRegion(point, oldColor));
+};
+
+const getAdjuscentPixels = function (id) {
+	let [x,y] = id.split(',').map(element => +element);
+	let adjuscentIds = [[x+1, y], [x-1, y], [x,y+1], [x,y-1]].map(list => list.join(','));
+	return adjuscentIds.map(getElement);
+};
+
+const selectTool = function (activeTool) {
+	let deactiveTool ;
+	[pencil, brush, deactiveTool] = (activeTool == 'pencil') 
+		? [true, false, 'brush'] 
+		: [false, true, 'pencil'];
+	getElement(activeTool).setAttribute('style', 'border: 2px solid red');
+	getElement(deactiveTool).style.border = '0px';
 };
